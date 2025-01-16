9bb4fdc6355703eb6bd4eaf1bc75d90e47f5d94d
Fehmi Baltaci <baltacifehmi@gmail.com>
2025-01-16 13:26:53 -0500

    Array of tasks added with for loop in component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import "./App.css";
 import TaskCard from "./components/TaskCard";
+import { Task } from "./types/Task";
+
+const tasks: Task[] = [
+  { title: "Conduct User Interviews", id: "UX-23", points: 8 },
+  { title: "Optimize Database Queries", id: "DB-17", points: 13 },
+  { title: "Design Landing Page", id: "UI-09", points: 5 },
+  { title: "Implement Authentication", id: "BE-12", points: 10 },
+];
 
 function App() {
   return (
     <>
-      <TaskCard task={{ title: "Conduct User Interviews", id: "UX-23", points: 8 }} />
-      <TaskCard task={{ title: "Optimize Database Queries", id: "DB-17", points: 13 }} />
-      <TaskCard task={{ title: "Design Landing Page", id: "UI-09", points: 5 }} />
-      <TaskCard task={{ title: "Implement Authentication", id: "BE-12", points: 10 }} />
+      {tasks.map((task) => (
+        <TaskCard key={task.id} task={task} />
+      ))}
     </>
   );
 }
