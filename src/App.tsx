af9271691a67cbe5ad388e2a92aec6e712d822f1
Fehmi Baltaci <baltacifehmi@gmail.com>
2025-01-15 20:55:47 -0500

    TaskCard in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import './App.css'
+import "./App.css";
 
 function App() {
+  const title: string = "Do Market Research";
+  const id: string = "BUS-1";
+  const points: number = 5;
 
   return (
-    <div>
-      <h1>Hello React TS Kanban Dashboard</h1>
+    <div className="border rounded-lg px-2 m-2 bg-gray-300">
+      <div className="text-base font-semibold py-2">{title}</div>
+      <div className="flex gap-4 justify-between">
+        <div>{id}</div>
+        <div>{points}</div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
